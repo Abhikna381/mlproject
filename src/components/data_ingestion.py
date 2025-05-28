26542d54ca21e5d42f8a38beab1dc07647917652
abhijeet <abhijeet.palai3@gmail.com>
2025-05-28 21:29:06 +0530

    DATA TRANSFORMATION IS COMPLETED

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass # to create classes with attributes
 
+from src.components.data_transformation import DataTransformationConfig,DataTransformation
 
 @dataclass
 class DataIngestionConfig:
@@ -49,5 +50,12 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
+
+    logging.info("Data ingestion and transformation completed successfully")
+
+    
 
