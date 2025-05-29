552e626fb27706ad29b39c5aaaeb9d54128c2a98
abhijeet <abhijeet.palai3@gmail.com>
2025-05-29 18:42:26 +0530

    MODEL TRAINER

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass # to create classes with attributes
 
 from src.components.data_transformation import DataTransformationConfig,DataTransformation
+from src.components.model_trainer import ModelTrainerConfig,ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -53,9 +54,16 @@ if __name__ == "__main__":
     train_data,test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data, test_data)
 
     logging.info("Data ingestion and transformation completed successfully")
+
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
+
+    logging.info("Model training completed successfully")
+
+    
 
     
 
